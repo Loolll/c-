77663450a27e03fe093d9bb35cb270fc1dfe3254
Loolll <pmvug4@inbox.ru>
2021-09-13 13:59:01 +0300

    init

---
+++ words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool exists(vector<auto> arr; auto object){
+    return find(arr.begin(), arr.end(), object)!=arr.end();
+}
+
+int main(){
+    vector<string> words;
+    for(string temp; cin>>temp;){
+        words.push_back(temp);
+    }
+
+    vector<string> blocked{"black", "women"};
+    sort(words.begin(), words.end());
+
+    for(string word:words){
+        if(blocked.
+    }
+}
